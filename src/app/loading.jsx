3ac526acc39850dd73d63aa97a39d810d95d1648
Hezekiah Elisha <elishahezekiah903@gmail.com>
2025-02-16 00:19:34 +0300

    Add loading animation component and integrate it into RecentListings for improved loading state handling

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,18 +1,6 @@
+import LoadingAnimation from "@/components/LoadingAnimation";
 import React from "react";
 
 export default function Loading() {
-  return (
-    <div className="card">
-      <div className="loader">
-        <p>loading</p>
-        <div className="words">
-          <span className="word">Properties</span>
-          <span className="word">Houses</span>
-          <span className="word">switches</span>
-          <span className="word">cards</span>
-          <span className="word">buttons</span>
-        </div>
-      </div>
-    </div>
-  );
+  return <LoadingAnimation />;
 }
--- src/components/RecentListings.jsx
+++ src/components/RecentListings.jsx
@@ -2,9 +2,12 @@
 import { useEffect, useState } from "react";
 import Listing from "@/components/Listing";
 import { instance } from "@/api";
+import LoadingAnimation from "./LoadingAnimation";
 
 export default function RecentListings() {
   const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     // Fetch recent listings from the server
     // Update the state with the fetched data
@@ -12,6 +15,7 @@ export default function RecentListings() {
       .get("/listings")
       .then((response) => {
         setListings(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error(error);
@@ -20,8 +24,13 @@ export default function RecentListings() {
   return (
     <div className="p-4">
       <h2 className="text-2xl">Recent Listings</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
         {listings.length === 0 && <p>No listings found</p>}
+        {loading && (
+          <div className="w-full flex justify-center items-center align-middle">
+            <LoadingAnimation />
+          </div>
+        )}
         {listings.map((listing) => (
           <Listing key={listing.id} listing={listing} />
         ))}
